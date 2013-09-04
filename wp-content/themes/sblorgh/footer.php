e9fe93d05cc83741f0cfe6336c759e0685bbaf0d
Richard Whittaker <pkeruno@gmail.com>
2013-09-04 23:14:02 +0100

    Fixes menubar positioning

--- wp-content/themes/sblorgh/footer.php
+++ wp-content/themes/sblorgh/footer.php
@@ -1,3 +1,11 @@
+			<div id="navbar" class="navbar">
+				<nav id="site-navigation" class="navigation main-navigation" role="navigation">
+					<h3 class="menu-toggle">Menu</h3>
+					<a class="screen-reader-text skip-link" href="#content" title="Skip to content">Skip to content</a>
+					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
+					<?php get_search_form(); ?>
+				</nav><!-- #site-navigation -->
+			</div><!-- #navbar -->
 		</div><!-- #main -->
 		<footer id="colophon" class="site-footer" role="contentinfo">
 
