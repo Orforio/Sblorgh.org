70812fe33b811c49a993657bade28a34a993eb5d
Richard Whittaker <pkeruno@gmail.com>
2013-09-05 00:36:02 +0100

    Start single page

--- wp-content/themes/sblorgh/single.php
+++ wp-content/themes/sblorgh/single.php
@@ -1,13 +1,4 @@
-<?php
-/**
- * The Template for displaying all single posts.
- *
- * @package WordPress
- * @subpackage Twenty_Thirteen
- * @since Twenty Thirteen 1.0
- */
-
-get_header(); ?>
+<?php get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
@@ -24,5 +15,4 @@ get_header(); ?>
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
