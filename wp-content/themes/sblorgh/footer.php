79f8ce3829115f018abfd238ae2798f71f688768
Richard Whittaker <pkeruno@gmail.com>
2013-09-04 22:15:23 +0100

    Fixes footer

--- wp-content/themes/sblorgh/footer.php
+++ wp-content/themes/sblorgh/footer.php
@@ -1,9 +1,8 @@
 		</div><!-- #main -->
 		<footer id="colophon" class="site-footer" role="contentinfo">
-			<?php get_sidebar( 'main' ); ?>
 
 			<div class="site-info">
-				&copy; 2012 <a href="/contacts/" title="Contact me">Richard Whittaker</a> | Powered by <a href="http://wordpress.org/" title="Semantic Personal Publishing Platform"> and <?php sblorgh_randomise_footer(); ?> | Amot Melak Matita</a>
+				&copy; 2012 <a href="/contacts/" title="Contact me">Richard Whittaker</a> | Powered by <a href="http://wordpress.org/" title="Semantic Personal Publishing Platform">Wordpress</a> and <?php echo sblorgh_randomise_footer(); ?> | Amot Melak Matita</a>
 			</div><!-- .site-info -->
 		</footer><!-- #colophon -->
 	</div><!-- #page -->
