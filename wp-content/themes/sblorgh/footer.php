1718f9ed9edf7e71781e6d710efd20d4fb21ce0f
Richard Whittaker <pkeruno@gmail.com>
2013-09-08 22:18:32 +0100

    Fixes footer

--- wp-content/themes/sblorgh/footer.php
+++ wp-content/themes/sblorgh/footer.php
@@ -1,6 +1,6 @@
 		</section>
 	</div>
-	<footer>
+	<footer class="page-footer">
 		&copy; <a href="/contacts/" title="Contact me">Richard Whittaker</a> | Powered by <a href="http://www.wordpress.org/" title="Visit WordPress.org">WordPress</a> and <?php echo sblorgh_randomise_footer(); ?> | Amot Melak Matita
 	</footer>
 <?php wp_footer(); ?>
